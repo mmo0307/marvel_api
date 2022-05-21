5a13b87573d40fe821e36718b6bd46ead39c7a8b
Misha <mmo030702@gmail.com>
2022-05-21 16:49:32 +0300

    Сhanged the appearance of the input form

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -24,12 +24,14 @@ const MainPage = () => {
                 <ErrorBoundary>
                     <CharList onCharSelected={onCharSelected}/>
                 </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-                </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharSearchForm/>
-                </ErrorBoundary>
+                <div>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharSearchForm/>
+                    </ErrorBoundary>
+                </div> 
             </div>
             <img className="bg-decoration" src={decoration} alt="vision"/>
         </>
